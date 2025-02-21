9b8e44a89f1dc36f90513a88d2b627b72bed411a
Nelson Kuchio <63778261+kuchisoft@users.noreply.github.com>
2025-02-21 16:41:45 +0300

    Update db.spec.ts
    Updated the query

--- e2e/src/tests/db.spec.ts
+++ e2e/src/tests/db.spec.ts
@@ -46,7 +46,7 @@ test("SQL query test @db", async ({}) => {
     JOIN Orders o ON u.user_id = o.user_id
     JOIN OrderItems oi ON o.order_id = oi.order_id
     JOIN Products p ON oi.product_id = p.product_id
-    WHERE p.product_name = 'Widget' AND o.order_date >= '2020-05-01'
+    WHERE p.product_name = 'Widget' AND o.order_date <= '2020-05-01'
     GROUP BY u.username
     HAVING SUM(oi.quantity) > 50;
   `;
